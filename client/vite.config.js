083017f850865247b604efebcf119611c7c814d9
PIYUSH PANCHABHAI <piyushpanchabhai200@gmail.com>
2024-07-29 14:43:58 +0530

    fa

--- client/vite.config.js
+++ client/vite.config.js
@@ -46,7 +46,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
     rollupOptions: {
-      external: ['react', 'react-dom','react-icons'],
+      external: ['react', 'react-dom','react-icons/fa'],
     },
   }
 });
