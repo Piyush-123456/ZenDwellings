0032a3af710395df7e19e6c31f2b63ee37571caf
PIYUSH PANCHABHAI <piyushpanchabhai200@gmail.com>
2024-07-29 13:10:33 +0530

    my last commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,31 @@
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false
+//       }
+//     },
+//   },
+
+//   plugins: [react(), sentryVitePlugin({
+//     org: "piyush-panchabhai",
+//     project: "javascript-react"
+//   })],
+
+//   build: {
+//     sourcemap: true
+//   }
+// })
+
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,6 +44,9 @@ export default defineConfig({
   })],
 
   build: {
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
   }
-})
+});
